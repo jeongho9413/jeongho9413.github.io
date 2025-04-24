293df37a315d26bbc4a01eb8a4f43dad00795fea
jeongho9413 <ahn@irvs.ait.kyushu-u.ac.jp>
2025-04-24 00:27:12 +0000

    Deploying to gh-pages from @ jeongho9413/jeongho9413.github.io@31f84fbd91749e0c78e717d0efa7e43e906a8b2f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -447,7 +447,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%68%6E@%69%72%76%73.%61%69%74.%6B%79%75%73%68%75-%75.%61%63.%6A%70", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -455,6 +455,20 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/jeongho-ahn-8726761ab", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0000-8676-7701", "_blank");
         },
       },{
         id: 'social-rss',
@@ -468,14 +482,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=noQtIJsAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
