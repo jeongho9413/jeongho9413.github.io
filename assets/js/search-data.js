b14f09251f4f7a1976a618107ffd3b3ef9cf832b
jeongho9413 <ahn@irvs.ait.kyushu-u.ac.jp>
2025-10-27 16:31:13 +0000

    Deploying to gh-pages from @ jeongho9413/jeongho9413.github.io@d778c888ce92a6bd99affdffa020c3751c846211 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "",
+          description: "Download CV (PDF)",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
